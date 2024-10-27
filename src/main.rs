8b8e33d0355ab1f61b6b23ea673d9eacadbfe026
Keys <70819367+keys-i@users.noreply.github.com>
2024-10-28 00:34:54 +0530

    Added Player to the game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+    {terminal, ExecutableCommand},
+};
+use invaders::{
+    frame::{self, new_frame, Drawable, Frame},
+    player::Player,
+    render,
+};
+use rusty_audio::Audio;
+use std::{
+    error::Error,
+    sync::mpsc::{self, Receiver},
+    time::Duration,
+    {io, thread},
+};
+
+fn render_screen(render_rx: Receiver<Frame>) {
+    let mut last_frame = frame::new_frame();
+    let mut stdout = io::stdout();
+    render::render(&mut stdout, &last_frame, &last_frame, true);
+    while let Ok(curr_frame) = render_rx.recv() {
+        render::render(&mut stdout, &last_frame, &curr_frame, false);
+        last_frame = curr_frame;
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    for sound in &["explode", "lose", "move", "pew", "startup", "pew", "win"] {
+        audio.add(sound, format!("sounds/{}.wav", sound));
+    }
+    audio.play("startup");
+
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        render_screen(render_rx);
+    });
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game Loop
+    let mut player = Player::new();
+    'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
+
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Draw & Render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    Ok(())
 }
